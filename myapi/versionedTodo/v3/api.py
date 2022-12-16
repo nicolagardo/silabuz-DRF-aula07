5ee4a0c4b256eb34dbd67a5d168489acc55a2691
nicolagardo <nicolagardo@gmail.com>
2022-12-16 18:04:07 -0300

    taller 08

--- myapi/versionedTodo/v3/api.py
+++ myapi/versionedTodo/v3/api.py
@@ -17,3 +17,51 @@ class TodoViewSet(viewsets.ModelViewSet):
 
     throttle_classes = [UserRateThrottle]
     # throttle_scope = 'get'
+
+
+
+    tarea = """
+    
+  
+Posterior a la implementación y prueba del ScopedRateThrottle, 
+nuestra última tarea de la unidad será la siguiente.
+
+API de pagos
+Esta api funcionará como un pequeño proyecto, en la cual deben 
+hacer uso de las siguientes tecnologías.
+
+simpleJWT
+
+Throttling
+
+Para esta API de pagos el enunciado es el siguiente.
+
+Se necesitará crear una aplicación de usuarios que permita el
+login como lo vimos anteriormente. Luego de esto, crear una aplicación pagos
+ la cual debe registrar el usuario, el servicio, el monto, y la fecha de pago.
+
+La aplicación de pagos solo debe admitir las siguiente operaciones:
+
+GET (list y retrieve)
+
+POST
+
+Debe contener paginación y filtros de búsqueda para los campos de user__id, 
+fecha_pago y servicio.
+
+Los servicios a usar dentro de la APP para registrar los pagos son lo siguientes:
+
+Netflix
+
+Amazon Video
+
+Star +
+
+Paramount +
+
+Recordar que esta aplicación y las vistas creadas, deben hacer uso de simpleJWT 
+para que se haga uso del login de los usuarios.
+
+Por último, la creación de pagos debe estar limitada a 1000 request por día.
+    
+    """
